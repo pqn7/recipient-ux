4d20327e7a18b8ce057eaa9fe2c93660ea92770e
Hamm <pqn7@cdc.gov>
2024-10-17 13:14:39 -0400

    rename file
    Signed-off-by: Hamm <pqn7@cdc.gov>

--- csv-rfc4180.py
+++ csv-rfc4180.py
@@ -4,6 +4,8 @@
 
 # resources:
 #   https://www.geeksforgeeks.org/get-file-size-in-bytes-kb-mb-and-gb-using-python/
+#   https://docs.python.org/3/library/csv.html
+#   https://www.ietf.org/rfc/rfc4180.txt
 
 #%%
 import csv
@@ -13,10 +15,10 @@ def init_read(file_name):
     # get filesize in bytes
     #file_name = "C:/Users/pqn7/OneDrive - CDC/projects/recipient-experience/from_chad/cdc_cfi_complete_20240827.csv"
     file_name_size_bytes = os.path.getsize(file_name)
-    file_name_size_third = file_name_size_bytes // 3 # read one third of file bytes
+    file_name_size_third = file_name_size_bytes # read a third of file bytes
     return file_name_size_third
 
-def sniff_csv(file_name, fileout, file_bytes):
+def sniff_csv(file_name, file_bytes):
 
     csvfile = open(file_name, "r")
     dialect = csv.Sniffer().sniff(csvfile.read(file_bytes))
@@ -26,15 +28,16 @@ def sniff_csv(file_name, fileout, file_bytes):
 
 def recipient_writer(file_name, fileout, file_bytes):
     with open(fileout, 'w') as csv_out:
-        dialect, reader = sniff_csv(file_name, fileout, file_bytes)
-        reader_in = csv.writer(csv_out, dialect=dialect, delimiter=',',
+        dialect, reader = sniff_csv(file_name, file_bytes)
+        reader_in = csv.writer(csv_out, dialect=dialect, delimiter=',', doublequote=True,
                                 quotechar='"', quoting=csv.QUOTE_MINIMAL, lineterminator='\r\n')
-                                
         reader_in.writerows(reader)
+
+
 
 if __name__ == "__main__":
     
-    file_name = "C:/Users/pqn7/OneDrive - CDC/projects/recipient-experience/from_chad/cdc_cfi_complete_20240827.csv"
-    file_out = "C:/Users/pqn7/OneDrive - CDC/projects/recipient-experience/from_chad/cdc_cfi_complete.csv"
+    file_name = "C:/Users/pqn7/OneDrive - CDC/projects/recipient-experience/from_chad/CDC Raw Data File _Combined_8.27_FINAL-no$-raw-data.csv"
+    file_out = "C:/Users/pqn7/OneDrive - CDC/projects/recipient-experience/from_chad/cfi-raw-data.csv"
     #dialect, reader = sniff_csv(file_name, init_read(file_name))
     recipient_writer(file_name, file_out, init_read(file_name))
